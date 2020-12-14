b27ec9ac01f47d3d84838a8258a2bb2696c4e69d
Hampus Nilsson <hampus10699@gmail.com>
2020-12-14 16:57:52 +0100

    added my own hero section

---
+++ bilgaraget/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Välkommen till BilGaraget</h1>
+      <p class="lead">Detta är ett forum för bilintresserade personer.</p>
+      <hr class="my-4">
+      <p>Detta forum är för allt som har med bilar att göra veteranbilar, ombyggda bilar, vanliga bilar etc.</p>
+      <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+}
+</script>
