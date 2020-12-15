bc23ba3a734c0565d56ffc616e091d61f433d36a
Hampus Nilsson <hampus10699@gmail.com>
2020-12-15 18:38:31 +0100

    imported materialize

--- bilgaraget/src/main.js
+++ bilgaraget/src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'materialize-css'
+
+import 'materialize-css/dist/css/materialize.min.css'
 
 Vue.config.productionTip = false
 
+//import 'bootstrap/dist/css/bootstrap.css'
 import "bootswatch/dist/cosmo/bootstrap.css"
 
 new Vue({
