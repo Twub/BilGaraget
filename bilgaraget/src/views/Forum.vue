e848f1e2dafca745e3ed1d9247d4c00d419b4020
Hampus Nilsson <hampus10699@gmail.com>
2020-12-16 00:38:27 +0100

    created a forum view

---
+++ bilgaraget/src/views/Forum.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="forum">
+    <h1>FORUM</h1>
+    <button @click="test">Click me!</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Forum',
+  props: ['categoryType'],
+  data: function(){
+      return {
+        category: '',
+      }
+    },
+    methods: {
+      test(){
+          console.log(this.categoryType)
+      }
+    },
+} 
+</script>
