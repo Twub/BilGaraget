b613dd04ec5436beda173258e324ca957a3a9310
Hampus Nilsson <hampus10699@gmail.com>
2020-12-16 00:59:39 +0100

    created a components that is going to list all threads.

---
+++ bilgaraget/src/components/ForumCategories/ListThreads.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="threads">
+        <h1>THREADS LIST</h1>
+        <h4>{{ this.category }}</h4>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'list-threads',
+    props: ['category'],
+    data: function(){
+      return {
+        threads: [],
+      }
+    },
+    methods: {
+      async getThreads(){
+          
+      }
+    },
+}
+</script>
+
+<style>
+
+</style>
