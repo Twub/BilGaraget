a1f68a3f37800ea799c7e90bdf561188a600f65d
Hampus Nilsson <hampus10699@gmail.com>
2020-12-17 03:46:23 +0100

    created a menu that contains button to create a new thread in the forum

---
+++ bilgaraget/src/components/ForumCategories/ForumMenu.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="forum-menu">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+            <button type="button" class="btn btn-primary" @click="$router.push({name: 'create-thread', params: {category: category}})">Skapa tråd</button>
+        </nav>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'forum-menu',
+    props: ['category'],
+}
+</script>
+
+<style>
+.forum-menu{
+    margin-bottom: 20px;
+}
+</style>
