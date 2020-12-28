021203c0602afe7d0484e1da55aae0f3a6f0a9ce
Hampus Nilsson <hampus10699@gmail.com>
2020-12-28 01:10:22 +0100

    added component for creating an account

---
+++ bilgaraget/src/views/CreateAccountView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="create-account-view">
+        <Signup></Signup>
+    </div>
+</template>
+
+<script>
+import Signup from '@/components/user/Signup'
+
+export default {
+    name: 'crate-account-view',
+    components: {
+        Signup,
+    }
+}
+</script>
+
+<style>
+
+</style>
